d5bbb45331396a45cbdb087075ddecacfbed711f
Pengzhi Quah <pengzhiq@gmail.com>
2011-09-10 21:14:54 +0000

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem 'rake', '~> 0.8.7'
+gem 'hirb'
+gem 'wirble'
+gem 'jquery-rails', '>= 1.0.12'
+gem 'httparty'
+
+group :development, :test do
+  gem "rspec-rails", "~> 2.6"
+  gem "factory_girl_rails"
+  gem "faker"
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = '8cf84bb8f8b4f403d21333a792c1d54917a946443af2cf64aff74434365dd06fd5d5314907128e8a198667fe22d176a1d061a5bbb9fed5b7e9a97b3c165af10f'
