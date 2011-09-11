06cf3352750efc8f6f72fbcc86e8732a8f04d948
Pengzhi Quah <pengzhiq@gmail.com>
2011-09-11 10:34:06 -0400

    make ID starts at 5000 so that base36 will become more alphanumeric

--- db/migrate/20110910221918_create_locators.rb
+++ db/migrate/20110910221918_create_locators.rb
@@ -4,6 +4,17 @@ class CreateLocators < ActiveRecord::Migration
       t.string :url
       t.timestamps
     end
+    
+    # make ID starts at 5000 so that base36 will become more alphanumeric
+    # http://stackoverflow.com/questions/692856/set-start-value-for-autoincrement-in-sqlite
+    
+    # apparently you have to create something first in order to update sqlite_sequence:
+    Locator.create( :url => "http://www.flickr.com/photos/amoschapple/5961342507/sizes/o/in/photostream/")
+    Locator.delete_all
+    
+    execute <<-SQL
+      UPDATE SQLITE_SEQUENCE SET seq = 5000 WHERE name = 'locators'
+    SQL
   end
 
   def self.down
